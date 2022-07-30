4192a0f33dc53e2791018799923fa177eeb0b1c6
turzoxpress1 <turzoxpress@gmail.com>
2022-07-30 13:38:16 +0600

    social link fixed

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -7,19 +7,19 @@ const SocialMedia = () => {
   return (
     <div className="app__social">
       <div>
-        <a href={constants.linkedin_link}>
+        <a href={constants.linkedin_link} target="_blank" rel="noreferrer">
           <FaLinkedin />
         </a>
       </div>
 
       <div>
-        <a href={constants.github_link}>
+        <a href={constants.github_link} target="_blank" rel="noreferrer">
           <FaGithub />
         </a>
       </div>
 
       <div>
-        <a href={constants.facebook_link}>
+        <a href={constants.facebook_link} target="_blank" rel="noreferrer">
           <BsFacebook />
         </a>
       </div>
